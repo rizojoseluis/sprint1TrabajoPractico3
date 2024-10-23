7c6d162b04b8cdfb0cffac7be68aac3a71bebe87
rizojoseluis <147005368+rizojoseluis@users.noreply.github.com>
2024-10-22 21:04:39 -0300

    Add files via upload

---
+++ index.mjs
@@ -0,0 +1,13 @@
+import { leerSuperheroes, agregarSuperheroes } from './utils.mjs';
+
+const archivoOriginal = './superheroes.txt';
+const archivoNuevos = './agregarSuperheroes.txt';
+
+// Agregar nuevos superhéroes
+agregarSuperheroes(archivoOriginal, archivoNuevos);
+
+// Leer y mostrar la lista actualizada de superhéroes ordenada
+const superheroes = leerSuperheroes(archivoOriginal);
+console.log( 'Superhéroes ordenados:');
+console.log(superheroes) ; 
+
